156d5047aa5bfce8905106c4b13f419b5474a49b
Karl Schmidt <karl.schmidt@thinprint.de>
2020-11-11 21:58:30 +0100

    WIP: sign debian package

--- build.sh
+++ build.sh
@@ -3,5 +3,9 @@ echo "remove old buildfiles"
 ./clean.sh
 echo "build"
 cd src
+
+debsign \
+  -p'gpg --passphrase-file passphrase_file.txt --batch --no-use-agent'\
+  -S -kMYKEYID debian/changelog
 dpkg-buildpackage
 echo "done"
